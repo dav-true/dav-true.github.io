791fd64ec0f240f85f747a0e552c5b2871f089a7
dav_true <danil.savolyuk@gmail.com>
2020-03-22 22:10:58 +0200

    com-1

--- js/script.js
+++ js/script.js
@@ -2,28 +2,26 @@
 
 $('document').ready(function () {
 
-  
-    async function vue () {
+
+    async function vue() {
         const dt = await loadData();
         let cities = dt.cities;
         let cinemas = dt.cinemas;
-        
 
-        Vue.component ("city-option", {
-            // data : {cities : cities},
-            props:{city: Object},
-            template: `<div>{{city.name}}</div>`
+
+        Vue.component("city-option", {
+            props: { city: Object },
+            template: `<div class="city">{{city.name}}</div>`
         })
-        
-        
+
+
         new Vue({
             el: "#city-choose-div",
-            data : {cities : cities}
-        })    
+            data: { cities: cities }
+        })
 
-        Vue.component ("cinema", {
-            // data : {cities : cities},
-            props:{cinema: Object},
+        Vue.component("cinema", {
+            props: { cinema: Object },
             template: ` <div class='cinema-block'>
                             <div class='cinema-photo-div' v-bind:style="{ backgroundImage: 'url(' + cinema.photo + ')' }"></div>
                             <div class='cinema-info-div'>
@@ -45,35 +43,51 @@ $('document').ready(function () {
         })
         new Vue({
             el: "#cinema-list",
-            data: {cinemas : cinemas} 
+            data: { cinemas: cinemas }
+
+        })
+
+
+        $('.city').click (function () {
             
         })
 
-        // var n = $('.tags').eq(0).attr('tags')
-        // console.log(n)
-        
+
         var temp = [];
-        
-        $('.cinema-class-tag').click(function() {
+
+        $('.cinema-class-tag').click(function () {
+
+            if($(this).attr('data-checked') ==  'false') {
+                $(this).css('background-color', "yellowgreen");
+                $(this).css('color', "white");
+                $(this).attr('data-checked', 'true')
+                $(this).css("border-color", "white");
+            } else {
+                $(this).css('background-color', "#dedede");
+                $(this).attr('data-checked', 'false')
+                $(this).css('color', "black");
+                $(this).css("border-color", "black");
+            }
+
             var clicked = $(this).text();
-            
-            if(temp.length == 0) {
+
+            if (temp.length == 0) {
                 temp.push(clicked);
                 console.log(temp);
             } else {
                 var temp_counter = 0;
-                for(n = 0; n < temp.length; n++) {
-                    if(temp[n] == clicked) {
+                for (n = 0; n < temp.length; n++) {
+                    if (temp[n] == clicked) {
                         temp_counter++;
                     }
-                } 
+                }
 
                 if (temp_counter == 0) {
                     temp.push(clicked);
                     console.log(temp)
                 } else {
                     const index = temp.indexOf(clicked)
-                    if(index > -1) {
+                    if (index > -1) {
                         temp.splice(index, 1)
                     }
 
@@ -81,26 +95,40 @@ $('document').ready(function () {
                 }
             }
 
-           
 
 
-            for(i = 0; i < $('.cinema').length; i++ ) {
-                var c = $('.cinema').eq(i).contents().find('.tags');
-                var counter = 0;
-                for(k = 0; k < c.length; k++ ){
-                    var tag = c.eq(k).text()
-                    
-                    if (tag == ' ' + clicked) {
-                        counter++;
+            var cinema_counter = 0; 
+            for (i = 0; i < $('.cinema').length; i++) {
+                if (temp.length > 0) {
+                    var c = $('.cinema').eq(i).contents().find('.tags');  /////////All tags in line
+                    var counter = 0;
+                    for (k = 0; k < c.length; k++) {
+                        for (l = 0; l < temp.length; l++) {
+                            var tag = c.eq(k).text()
+                            if (tag == ' ' + temp[l]) {
+                                counter++;
+                            }
+                        }
                     }
-                } 
-                
-                if (counter == 0) {
-                    $('.cinema').eq(i).css('display', 'none')
-                } else {
-                    $('.cinema').eq(i).css('display', 'flex')
+
+                    if (counter < temp.length) {
+                        cinema_counter++;
+                        $('.cinema').eq(i).css('display', 'none')
+                    } else if (counter >= temp.length) {
+                        $('.cinema').eq(i).css('display', 'flex')
+                    }
+                    
+                } else if (temp.length == 0) {
+                    for (i = 0; i < $('.cinema').length; i++) {
+                        $('.cinema').eq(i).css('display', 'flex')
+                    }
                 }
-                
+            } 
+
+            if(cinema_counter == $(".cinema").length) {
+                $('#test').css("display", "block")
+            } else {
+                $('#test').css("display", "none")
             }
         })
 
@@ -109,8 +137,8 @@ $('document').ready(function () {
 
     vue()
 
-    
-    
+
+
     var ln = $('.poster').length;
     var groups = ln / 3;
     var modal = $('.modal-div')[0];
@@ -123,7 +151,7 @@ $('document').ready(function () {
         if (res.ok) {
             console.log("ok");
         } else {
-            console.log ("ga");
+            console.log("ga");
         }
         return res.json();
     }
@@ -135,7 +163,7 @@ $('document').ready(function () {
         var time;
 
 
-        if(i == 0) {
+        if (i == 0) {
             target = "#movies";
             time = 400;
         } else if (i == 1) {
@@ -149,45 +177,45 @@ $('document').ready(function () {
 
         console.log(target)
         $('html, body').stop().animate({
-            scrollTop : $(target).offset().top
+            scrollTop: $(target).offset().top
         }, time)
     }
-    
-    
-    $('.nav-butt').click(function (){         ////////////////////// Animation of moving to dif pages blocks
+
+
+    $('.nav-butt').click(function () {         ////////////////////// Animation of moving to dif pages blocks
         slideToElement($(".nav-butt").index(this));
     })
-    $('.nav-butt-drop').click(function (){         ////////////////////// Animation of moving to dif pages blocks
+    $('.nav-butt-drop').click(function () {         ////////////////////// Animation of moving to dif pages blocks
         slideToElement($(".nav-butt-drop").index(this));
     })
 
 
 
-    for (i = 0; i < groups; i++ ) {           ///////////////////////// Different margin-top when different amount of posters in a row
-        if(i % 2 == 0) {
-            for(k = i * 3 ; k < i * 3 + 3; k++ ) {
-                if(k % 2 == 0) {
+    for (i = 0; i < groups; i++) {           ///////////////////////// Different margin-top when different amount of posters in a row
+        if (i % 2 == 0) {
+            for (k = i * 3; k < i * 3 + 3; k++) {
+                if (k % 2 == 0) {
                     $('.poster').eq(k).css('margin-top', '120px');
-                    
+
                 } else {
                     $('.poster').eq(k).css('margin-top', '60px');
-                    
+
                 }
             }
 
         } else if (i % 2 == 1) {
-            for(k = i * 3 ; k < i * 3 + 3; k++ ) {
-                if(k % 2 == 0) {
+            for (k = i * 3; k < i * 3 + 3; k++) {
+                if (k % 2 == 0) {
                     $('.poster').eq(k).css('margin-top', '60px');
-                
+
                 } else {
                     $('.poster').eq(k).css('margin-top', '120px');
-                    
+
                 }
             }
-        } 
+        }
     }
-    
+
     $(".poster-div").mouseenter(function () {       /////////////////////// Poster blur
         var i = $(this).index();
         $(".centered").eq(i).fadeTo(300, 1)
@@ -200,90 +228,90 @@ $('document').ready(function () {
     $(".mark").mouseenter(function () {            ///////////////////////  Marks sliding up and sliding down on hover
         var i = $(this).index();
         $('.mark').eq(i).css('animation', 'slideUp .4s forwards')
-        $('.mark::after').eq(i).fadeTo(300,1)
+        $('.mark::after').eq(i).fadeTo(300, 1)
     })
-   
+
     $(".mark").mouseleave(function () {
         var i = $(this).index();
         $('.mark').eq(i).css('animation', 'slideDown .4s forwards')
-        $('.mark::after').eq(i).fadeTo(300,0)
+        $('.mark::after').eq(i).fadeTo(300, 0)
     })
-   
-  
 
-    window.onclick = function(event) {  ///////////////////////////// Modal div closing by clicking ourside the block
+
+
+    window.onclick = function (event) {  ///////////////////////////// Modal div closing by clicking ourside the block
         if (event.target == modal) {
             modal.style.opacity = "0";
             modal.style.display = "none";
 
             let target = $('#trailer-iframe');
-        
+
             target.attr('src', '');
-          
-        } 
-      }
-    
-    $(document).click (function(event) {
-        if(!$(event.target).hasClass('targeter')) {
+
+        }
+    }
+
+    $(document).click(function (event) {
+        if (!$(event.target).hasClass('targeter') && $("#cities-options-div").attr('data-opened') == "true") {
             $('#cities-options-div').css('animation', 'dropDownOut 0.4s forwards');
             $('#cities').attr('data-check', "false");
             $("#cities-arrow").css('animation', 'downUpRotate 0.4s forwards')
         }
-        if(!$(event.target).hasClass('targeter_2')) {
+        if (!$(event.target).hasClass('targeter_2')) {
             $('.dropped-menu').css('animation', 'fadeOutRight 0.4s forwards');
-           
+
         }
     })
-    
-    function screenWidth () {          ////////////////////////////  Resize Poster-div by changing screen width
+
+    function screenWidth() {          ////////////////////////////  Resize Poster-div by changing screen width
         if ($(window).width() >= 1283) {
             $('.dropped-menu').css('display', 'none')
-            for (i = 0; i < ln; i++ ) {
-                if(i % 3 == 0 ) {
-                   $('.poster-div').eq(i).css("margin-left", '0')
+            for (i = 0; i < ln; i++) {
+                if (i % 3 == 0) {
+                    $('.poster-div').eq(i).css("margin-left", '0')
                 } else {
                     $('.poster-div').eq(i).css("margin-left", '150px')
                 }
-            } 
-        } else if($(window).width() <= 1282 && $(window).width() >= 1093 ) {
+            }
+        } else if ($(window).width() <= 1282 && $(window).width() >= 1093) {
             $('.dropped-menu').css('display', 'none')
-            for (i = 0; i < ln; i++ ) {
-                if(i % 2 == 0 ) {
-                   $('.poster-div').eq(i).css("margin-left", '0')
+            for (i = 0; i < ln; i++) {
+                if (i % 2 == 0) {
+                    $('.poster-div').eq(i).css("margin-left", '0')
                 } else {
                     $('.poster-div').eq(i).css("margin-left", '250px')
                 }
-            } 
-         
-        } else if($(window).width() <= 1094 && $(window).width() >= 893){
+            }
+
+        } else if ($(window).width() <= 1094 && $(window).width() >= 893) {
             $('.dropped-menu').css('display', 'none')
-            for (i = 0; i < ln; i++ ) {
-                if(i % 2 == 0 ) {
-                   $('.poster-div').eq(i).css("margin-left", '0')
+            for (i = 0; i < ln; i++) {
+                if (i % 2 == 0) {
+                    $('.poster-div').eq(i).css("margin-left", '0')
                 } else {
                     $('.poster-div').eq(i).css("margin-left", '100px')
                 }
-            } 
+            }
         } else if ($(window).width() <= 892) {
             $('.poster-div').css("margin-left", '20%')
         }
     }
 
 
-    $(window).resize(function() {   
+    $(window).resize(function () {
         console.log($(window).width())
-       screenWidth();
+        screenWidth();
     })
 
     screenWidth();
 
-    $('.centered').click (async function () {             //////////////////////// Poster name clicking function
+    $('.centered').click(async function () {             //////////////////////// Poster name clicking function
         var id = $(this).attr('data-jsonid');
-        console.log(id);  
-      
+        console.log(id);
+
         var data = await loadData();
         let target = $('#trailer-iframe');
-        
+
         target.attr('src', data.movies[id].url);
         $('#modal-movie-title').text(data.movies[id].title)
         $('#movie-year').text(data.movies[id].year);
@@ -296,31 +324,33 @@ $('document').ready(function () {
         $('#description').text(data.movies[id].description);
 
         $('.modal-div').css('display', 'block');
-        $('.modal-div').fadeTo(400,1)
-       
+        $('.modal-div').fadeTo(400, 1)
+
     })
 
 
     $("#cities").click(() => {                  ///////////////////////////// Cities drop down list animationW
-        if($('#cities').attr('data-check') == "false") {
+        if ($('#cities').attr('data-check') == "false") {
             $('#cities').attr('data-check', "true");
+            $("#cities-options-div").attr('data-opened', 'true');
             $('#cities-options-div').css('animation', 'dropDown 0.4s forwards');
             $("#cities-arrow").css('animation', 'upDownRotate 0.4s forwards')
         } else if ($('#cities').attr('data-check') == 'true') {
             $('#cities-options-div').css('animation', 'dropDownOut 0.4s forwards');
             $('#cities').attr('data-check', "false");
             $("#cities-arrow").css('animation', 'downUpRotate 0.4s forwards')
+            $("#cities-options-div").attr('data-opened', 'false');
         }
 
     })
 
 
-    $('.close-butt').click (function () {
-        
+    $('.close-butt').click(function () {
+
         $('.dropped-menu').css('animation', 'fadeOutRight 0.4s forwards');
     })
 
-    $('.menu-butt').click (function () {
+    $('.menu-butt').click(function () {
         $('.dropped-menu').css('display', 'block');
         $('.dropped-menu').css('animation', 'fadeInRight 0.4s forwards')
     })
@@ -328,21 +358,3 @@ $('document').ready(function () {
 })
 
 
-
-
-
-
-    // Vue.component ("city-option", {
-    //     template: `<p>{{city.name}}</p>`
-    // })
-    // const cities = [
-    //     {id:0, name:"kyiv"},
-    //     {id:0, name:"kyiv"},
-    //     {id:0, name:"kyiv"}
-    // ];
-
-    
-    // new Vue({
-    //     el: "#app",
-    //     data: {cities : cities}
-    // })  
